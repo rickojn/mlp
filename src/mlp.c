cb49d81a4ff0051933ba7c789507bd53c7885a3a
rickojn <rickojn@hotmail.com>
2024-11-10 14:05:32 +0000

    debugging

--- src/mlp.c
+++ src/mlp.c
@@ -247,9 +247,11 @@ void model_forward(Model * model, TrainingSet * training_set ){ // need to chang
 }
 
 float cross_entropy_loss(float * probs, char * labels, size_t size_batch){
+    printf("\nreal batch size = %zu\n", size_batch);
     float batch_loss = 0.0;
-    // for (int i = 0; i < size_batch; i++){
-    for (int i = 0; i < 32; i++){
+    // size_batch = 220000;
+    printf("\nused batch size = %zu\n", size_batch);
+    for (int i = 0; i < size_batch; i++){
          batch_loss += log(probs[(i * SIZE_VOCAB) + encode(labels[i])]) * -1;
     }
     return batch_loss/size_batch;
