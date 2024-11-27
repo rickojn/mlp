eed7a8a437e9dc182efd4316f32a094900907845
rickojn <rickojn@hotmail.com>
2024-11-27 12:40:49 +0000

    emb bp wip

--- src/mlp.c
+++ src/mlp.c
@@ -302,12 +302,20 @@ void matmul_backward(const float * grads_z, float * grads_w, float * grads_b, co
 }
 
 
-void embedding_backwards(float * embedding_activations, float * grads_w, char * inputs, size_t size_activations, 
-size_t  size_grads_w, size_t size_inputs){
-    for (size_t idx_input =0; idx_input < size_inputs; idx_input++ ){
-        
+void embedding_backwards(float * grads_z, float * grads_w, const char * inputs, size_t size_activations, 
+size_t  size_grads_w, size_t size_inputs, size_t size_batch){
+    for (size_t idx_batch = 0; idx_batch < size_batch; idx_batch++){
+        for (size_t idx_input = 0; idx_input < size_inputs; idx_input++){
+            size_t offset_input = idx_batch * size_inputs + idx_input;
+            size_t offset_grad_w = idx_batch * size_grads_w + encode(inputs[offset_input]) * DIM_EMBEDDINGS;
+            size_t offset_grad_z = idx_batch * size_activations + idx_input * DIM_EMBEDDINGS;
+            for (size_t idx_dim = 0; idx_dim < DIM_EMBEDDINGS; idx_dim++){
+                grads_w[offset_grad_w + idx_dim] = grads_z[offset_grad_z + idx_dim];
+            }
+        }
     }
 }
+
 
 
 void model_backwards(Model * model, TrainingSet * training_set){
