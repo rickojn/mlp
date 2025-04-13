bdc1d80a538282432321c749bd224486e60ed3fa
rickojn <rickojn@hotmail.com>
2025-04-13 11:46:37 +0100

    init layers

--- src/deep.c
+++ src/deep.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 #define SIZE_CLASSES 10
-#define SIZE_BATCH 100
+#define SIZE_MINI_BATCH 100
 #define SIZE_OUTPUT 10
 #define SIZE_HIDDEN 128
 
@@ -61,6 +61,73 @@ void read_mnist_labels(const char *filename, unsigned char **labels, int *nLabel
     fclose(file);
 }
 
+float generate_normal_random_number()
+{
+    // 2 uniformly distributed random numbers between 0 and 1
+    float u1 = ((float)rand() / RAND_MAX);
+    float u2 = ((float)rand() / RAND_MAX);
+
+    // use Box-Muller to return a normally distributed number
+    return sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+}
+
+// Xavier (Glorot) Normal Initialization
+float generate_xavier_number(size_t inputs, size_t outputs)
+{
+    float stddev = sqrt(2.0f / (inputs + outputs));
+    return generate_normal_random_number() * stddev;
+}
+
+// Kaiming Normal Initialization
+float generate_kaiming_number(size_t inputs, size_t outputs)
+{
+    float stddev = sqrt(2.0f / inputs);
+    return generate_normal_random_number() * stddev;
+}
+
+void kaiming_initialize_layer(Layer *layer, size_t inputs, size_t outputs)
+{
+    layer->size_inputs = inputs;
+    layer->size_outputs = outputs;
+
+    layer->weights = malloc(inputs * outputs * sizeof(float));
+    layer->biases = malloc(outputs * sizeof(float));
+
+    for (size_t i = 0; i < inputs * outputs; i++)
+        layer->weights[i] = generate_xavier_number(inputs, outputs);
+
+    for (size_t i = 0; i < outputs; i++)
+        layer->biases[i] = 0.0f;
+}
+
+void xavier_initialize_layer(Layer *layer, size_t inputs, size_t outputs)
+{
+    layer->size_inputs = inputs;
+    layer->size_outputs = outputs;
+
+    layer->weights = malloc(inputs * outputs * sizeof(float));
+    layer->biases = malloc(outputs * sizeof(float));
+
+    for (size_t i = 0; i < inputs * outputs; i++)
+        layer->weights[i] = generate_xavier_number(inputs, outputs);
+
+    for (size_t i = 0; i < outputs; i++)
+        layer->biases[i] = 0.0f;
+}
+
+
+
+void free_layer(Layer *layer)
+{
+    free(layer->weights);
+    free(layer->biases);
+}
+void free_model(Model *model)
+{
+    for (size_t i = 0; i < model->size_layers; i++)
+        free_layer(&model->layers[i]);
+    free(model->layers);
+}
 
 int main() {
     // read input data
@@ -81,6 +148,16 @@ int main() {
     // create model
     Model model;
     Layer layer_hidden, layer_output;
+    kaiming_initialize_layer(&layer_hidden, SIZE_CLASSES, SIZE_HIDDEN);
+    xavier_initialize_layer(&layer_output, SIZE_HIDDEN, SIZE_OUTPUT);
+
+
+    free_layer(&layer_hidden);
+    free_layer(&layer_output);
+    free(data_training.images);
+    free(data_training.labels);
+    free(data_test.images);
+    free(data_test.labels);
 
 
 
