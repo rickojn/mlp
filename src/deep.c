a92a6da973dee3d4959965f89803776f18f81fdd
rickojn <rickojn@hotmail.com>
2025-04-28 08:22:19 +0100

    op issue wip

--- src/deep.c
+++ src/deep.c
@@ -94,7 +94,7 @@ void matmul_forward(Layer *layer, float *input, float *output, size_t size_batch
             // output[idx_image * layer->size_neurons + idx_neuron] = layer->biases[idx_neuron];
             for (size_t idx_input = 0; idx_input < layer->size_inputs; idx_input++) {
                 output[idx_image * layer->size_neurons + idx_neuron] += 
-                layer->weights[idx_input * layer->size_neurons + idx_neuron] * input[idx_image * layer->size_inputs + idx_input];
+                input[idx_image * layer->size_inputs + idx_input] * layer->weights[idx_neuron * layer->size_neurons + idx_neuron];
             }
         }
     }
@@ -250,9 +250,9 @@ void model_forward(Model *model, Activations *activations, InputData *data)
 {
     for (size_t idx_layer = 0; idx_layer < model->size_layers; idx_layer++) {
         Layer *layer = model->layers[idx_layer];
-        // matmul_forward(layer, layer->activations_input, layer->activations_output, data->nImages);
+        matmul_forward(layer, layer->activations_input, layer->activations_output, data->nImages);
         // matmul_forward_tiling(layer, layer->activations_input, layer->activations_output, data->nImages);
-        matmul_forward_outer_product(layer, data->nImages);
+        // matmul_forward_outer_product(layer, data->nImages);
         layer->activation_forward(layer, data->nImages);
     }
 }
