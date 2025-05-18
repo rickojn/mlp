b9c4cd136660392bf9dafff75b39e5ee2fcc9f2a
rickojn <rickojn@hotmail.com>
2025-05-18 23:20:50 +0100

    simd

--- src/deep.c
+++ src/deep.c
@@ -397,10 +397,10 @@ void model_forward(Model *model, Activations *activations, InputData *data)
 {
     for (size_t idx_layer = 0; idx_layer < model->size_layers; idx_layer++) {
         Layer *layer = model->layers[idx_layer];
-    //    matmul_forward(layer, layer->activations_input, layer->activations_output, data->nImages);
+        matmul_forward(layer, layer->activations_input, layer->activations_output, data->nImages);
         // matmul_forward_tiling(layer, layer->activations_input, layer->activations_output, data->nImages);
         // matmul_forward_outer_product(layer, data->nImages);
-        simd_matmul_forward(layer, data->nImages);
+        // simd_matmul_forward(layer, data->nImages);
         printf("layer %zu: activations_output[11] = %f\n", idx_layer, layer->activations_output[11]);
         layer->activation_forward(layer, data->nImages);
     }
